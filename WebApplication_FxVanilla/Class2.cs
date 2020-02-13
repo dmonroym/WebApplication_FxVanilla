c366ddebf74455b65e4a2f9ca414deca2639bcda
Daniel Monroy <dmon@microsoft.com>
2020-02-13 13:17:00 -0800

    Add project files.

---
+++ WebApplication_FxVanilla/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_FxVanilla
+{
+    public class Class2
+    {
+        Class1 c = new Class1();
+    }
+}
